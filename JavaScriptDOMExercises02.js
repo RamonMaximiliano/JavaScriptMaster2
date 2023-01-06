d082d7f99d11ed4333877a0cbe86bc696f076cf8
Ramon Maximiliano Wilbert <ramon_max1986@hotmail.com>
2023-01-06 15:20:17 -0300

    error added

--- JavaScriptDOMExercises02.js
+++ JavaScriptDOMExercises02.js
@@ -1,9 +1,6 @@
-
 /* 
 https://www.youtube.com/watch?v=qQy5K-pE8Fo&list=PLpc_YvcwbxaSn6jn0VaTcG8A0Grgs1GSB&index=12&ab_channel=codebubb 
 */
-
-
 
 /*
   Exercises 01
@@ -11,23 +8,23 @@ https://www.youtube.com/watch?v=qQy5K-pE8Fo&list=PLpc_YvcwbxaSn6jn0VaTcG8A0Grgs1
   Add a label to each of the input fields: username, password, confirm password
 */
 
-let user = document.getElementById("username")
-let userLabel = "<label for='username' name='username'>Username</label><br>"
-user.insertAdjacentHTML("beforebegin", userLabel)
+let user = document.getElementById("username");
+let userLabel = "<label for='username' name='username'>Username</label><br>";
+user.insertAdjacentHTML("beforebegin", userLabel);
 
-let Password = document.getElementById("Password")
-let PasswordLabel = "<label for='Password' name='Password'>Password</label><br>"
-Password.insertAdjacentHTML("beforebegin", PasswordLabel)
+let Password = document.getElementById("Password");
+let PasswordLabel =
+  "<label for='Password' name='Password'>Password</label><br>";
+Password.insertAdjacentHTML("beforebegin", PasswordLabel);
 
-let ConfirmPassword = document.getElementById("ConfirmPassword")
-let ConfirmPasswordLabel = "<label for='ConfirmPassword' name='ConfirmPassword'>ConfirmPassword</label><br>"
-ConfirmPassword.insertAdjacentHTML("beforebegin", ConfirmPasswordLabel)
+let ConfirmPassword = document.getElementById("ConfirmPassword");
+let ConfirmPasswordLabel =
+  "<label for='ConfirmPassword' name='ConfirmPassword'>ConfirmPassword</label><br>";
+ConfirmPassword.insertAdjacentHTML("beforebegin", ConfirmPasswordLabel);
 
-let submit = document.getElementById("submit")
-let submitSpace = "<br>"
-submit.insertAdjacentHTML("beforebegin", submitSpace)
-
-
+let submit = document.getElementById("submit");
+let submitSpace = "<br>";
+submit.insertAdjacentHTML("beforebegin", submitSpace);
 
 /*
   Exercise 02
@@ -35,6 +32,44 @@ submit.insertAdjacentHTML("beforebegin", submitSpace)
   Add a required validation to each input that shows an error message next to the entry if it does not have any text entered.
 */
 
+function submitForm() {
+  let user = document.getElementById("username").value;
+  let Password = document.getElementById("Password").value;
+  let ConfirmPassword = document.getElementById("ConfirmPassword").value;
+
+  let removing = document.getElementById("usernameremove");
+  let Passwordremove = document.getElementById("Passwordremove");
+  let ConfirmPasswordremove = document.getElementById("ConfirmPasswordremove");
+
+  if (removing !== null) {
+    removing.remove();
+  }
+  if (Passwordremove !== null) {
+    Passwordremove.remove();
+  }
+  if (ConfirmPasswordremove !== null) {
+    ConfirmPasswordremove.remove();
+  }
+
+  if (user == "") {
+    let userTag = document.getElementById("username");
+    let userLabel =
+      " <label for='username' name='username' id='usernameremove'>Please type something for 'username'!</label>";
+    userTag.insertAdjacentHTML("afterend", userLabel);
+  }
+  if (Password == "") {
+    let PasswordTag = document.getElementById("Password");
+    let PasswordLabel =
+      " <label for='username' name='username' id='Passwordremove'>Please type something for 'Password'!</label>";
+    PasswordTag.insertAdjacentHTML("afterend", PasswordLabel);
+  }
+  if (ConfirmPassword == "") {
+    let ConfirmPasswordTag = document.getElementById("ConfirmPassword");
+    let ConfirmPasswordLabel =
+      " <label for='username' name='username' id='ConfirmPasswordremove'>Please type something for 'ConfirmPassword'!</label>";
+    ConfirmPasswordTag.insertAdjacentHTML("afterend", ConfirmPasswordLabel);
+  }
+}
 
 /*
   Exercise 03
@@ -48,12 +83,8 @@ submit.insertAdjacentHTML("beforebegin", submitSpace)
 Ensure the ‘Register’ button is disabled until the user has entered valid data into all the input fields.  Once they have, the registration button should then be enabled.
 */
 
-
 /*
   Exercise 05
   -----------
 When the user clicks the ‘Register’ button, a message should be displayed informing them of a successful user registration.
 */
-
-
-
